b038e12ff7db715e4885cb990932cb8c18985ef9
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-20 16:13:52 +0300

    Add heapsort function

--- sorting.cpp
+++ sorting.cpp
@@ -30,3 +30,18 @@ void heapify(double arr[], int n, int i) {
         heapify(arr, n, largest);
     }
 }
+
+// Main function to sort array using HeapSort
+void heapSort(double arr[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i >= 0; i--) {
+        swap(arr[0], arr[i]); // Move current root to end
+
+        // Call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
