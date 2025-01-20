69f89842109f7813898d5b6005bb77565f799b53
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-20 16:07:25 +0300

    Add heapify function

--- sorting.cpp
+++ sorting.cpp
@@ -7,3 +7,26 @@ void swap(double &a, double &b) {
     a = b;
     b = temp;
 }
+
+// Function to restore heap property
+void heapify(double arr[], int n, int i) {
+    int largest = i; // Initialize largest as root
+    int left = 2 * i + 1; // Left child index
+    int right = 2 * i + 2; // Right child index
+
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != i) {
+        swap(arr[i], arr[largest]); // Swap
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
